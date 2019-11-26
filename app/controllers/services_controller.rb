215fd660e5b11bfbe285042a64d7e9aa215f6541
valentin lmr <valentinlmr28@gmail.com>
2019-11-26 14:37:41 +0100

    service controler

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,8 @@
+class ServicesController < ApplicationController
+  def index
+    # @services = Service.all.group(:category)
+    @services = Service.select("category").group(:category)
+    # @service = Service.where(:categroy => @services)
+    @options = Service.all.order(category: :asc)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+Service.create!(name: 'Netflix Premium',  number_of_places: '4', total_price: '14', category: 'VOSD')
+Service.create!(name: 'Netflix Standar',  number_of_places: '2', total_price: '10', category: 'VOSD')
+Service.create!(name: 'OCS',              number_of_places: '4', total_price: '12', category: 'VOSD')
+Service.create!(name: 'Spotify family',   number_of_places: '6', total_price: '14', category: 'Music')
+Service.create!(name: 'Deezer Premium',   number_of_places: '6', total_price: '14', category: 'Music')
+Service.create!(name: 'Apple TV Premium', number_of_places: '4', total_price: '12', category: 'VOSD')
