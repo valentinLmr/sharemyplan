1bf621dd08723b7b42a477c6504ed506325b5ba5
Remi Cordonnier <remi.cordo@gmail.com>
2019-11-26 12:42:15 +0100

    ajout du controller subscriptions et des methodes associées

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'subscriptions/index'
+  get 'subscriptions/new'
+  get 'subscriptions/create'
+  get 'subscriptions/edit'
+  get 'subscriptions/update'
+  get 'subscriptions/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
