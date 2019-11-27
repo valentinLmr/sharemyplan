10707197647106009bb5e1fa082ae56768595161
Olivier Mathey <omathey@skynet.be>
2019-11-27 12:10:01 +0100

    petite sauvegarde en passant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # , except[:index]
   # ci dessous grise mis en comment suite pb merge
-  # before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Pundit: white-list approach.
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
