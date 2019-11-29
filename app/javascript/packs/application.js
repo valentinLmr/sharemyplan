c1c42ab5cc30b2ee39eac66064ee3b49c11f63cd
valentin lmr <valentinlmr28@gmail.com>
2019-11-29 12:46:29 +0100

    change a bit front: navbar bacjground

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@ import "bootstrap";
 import { categoryClicker } from "./plugins/add";
 import { changePlace } from "./plugins/change";
 import { someChange } from "./plugins/subscriptionIndex";
+import { stickyNavigation } from "./plugins/navbar";
+
 
 
 categoryClicker();
 changePlace();
 someChange();
+stickyNavigation();
