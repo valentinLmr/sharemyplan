dc4c1a1c309b647ba14896e8d615c2592c9f0068
Olivier Mathey <omathey@skynet.be>
2019-12-02 17:19:52 +0100

    virer m2

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     cotisation = Cotisation.find_by(checkout_session_id: event.data.object.id)
     cotisation.update(state: 'paid')
+    raise
   end
 end
