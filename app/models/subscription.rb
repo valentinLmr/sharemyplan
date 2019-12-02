e758aed22ca45fb226efd377fc6edde00669c7e0
valentin lmr <valentinlmr28@gmail.com>
2019-12-02 10:53:18 +0100

    reviews

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :service
   has_many :cotisations, dependent: :destroy
+  has_many :reviews, through: :cotisations
 
   validates :available_places, presence: true
   validates :identifiant, presence: true
@@ -14,4 +15,5 @@ class Subscription < ApplicationRecord
   def price
     self.service.total_price.fdiv(self.service.number_of_places).round(2)
   end
+
 end
