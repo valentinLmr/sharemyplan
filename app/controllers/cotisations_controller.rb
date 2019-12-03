650911a4e3637b1fc879da42fc378cf3c856a5ce
Olivier Mathey <omathey@skynet.be>
2019-12-03 12:11:57 +0100

    paiement ok

--- app/controllers/cotisations_controller.rb
+++ app/controllers/cotisations_controller.rb
@@ -45,20 +45,20 @@ class CotisationsController < ApplicationController
 
       init_payment
 
-      session = Stripe::Checkout::Session.create(
-        payment_method_types: ['card'],
-        line_items: [{
-          name: @cotisation.subscription.name,
-          # images: [@cotisation.service.photo_url],
-          # images: [@cotisation.service.photo_url],
-          amount: @cotisation.price_cents,
-          currency: 'eur',
-          quantity: 1
-        }],
-        success_url: cotisation_url(@cotisation),
-        cancel_url: cotisation_url(@cotisation)
-      )
-      @cotisation.update(checkout_session_id: session.id)
+      # session = Stripe::Checkout::Session.create(
+      #   payment_method_types: ['card'],
+      #   line_items: [{
+      #     name: @cotisation.subscription.name,
+      #     # images: [@cotisation.service.photo_url],
+      #     # images: [@cotisation.service.photo_url],
+      #     amount: @cotisation.price_cents,
+      #     currency: 'eur',
+      #     quantity: 1
+      #   }],
+      #   success_url: cotisation_url(@cotisation),
+      #   cancel_url: cotisation_url(@cotisation)
+      # )
+      # @cotisation.update(checkout_session_id: session.id)
 
       cagnotte_update
 
